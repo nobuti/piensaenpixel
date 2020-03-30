d7093e0cbc462b96aaa13606e8ec02dee5ab3974
nobuti <buti@nobuti.com>
2020-03-30 18:39:44 +0200

    Private route component

---
+++ src/components/routes/Private.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Redirect } from "@reach/router";
+
+import config from "../../config";
+import store from "../../lib/store";
+
+const PrivateRoute = ({ children }) => {
+  const isLoggedIn = store.get(config.sessionKey);
+
+  if (!isLoggedIn) {
+    return <Redirect to="/login" noThrow />;
+  }
+
+  return children;
+};
+
+export default PrivateRoute;
---
+++ src/config/index.js
@@ -0,0 +1,3 @@
+export default {
+  session: "piensaenpixel:session",
+};
